b03037ca5fa2a001707c3ba6ed173fae9ab08203
suhailshaik <shaiksuhail.ui@gmail.com>
2015-09-26 04:05:58 -0400

    intial Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name uqbahFoundationApp
+ * @description
+ * # uqbahFoundationApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('uqbahFoundationApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
